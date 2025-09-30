f2f0c618e8bc68d8cc569e13ea9527b21092108d
Andrey Demov <demovas@yandex.ru>
2025-09-30 17:04:02 +0300

    Добавлена пакет для функций, работающих со списками

--- myfuncs/strings.py
+++ myfuncs/strings.py
@@ -1,6 +1,8 @@
 def get_quoted_text(text, open_s=171, close_s=187):
     """
+
     Возвращает слово или текст, заключенный в пару символов
+
     :param text: входное слово или текст
     :param open_s: код открывающего символа
     :param close_s: код закрывающего символа
@@ -10,4 +12,4 @@ def get_quoted_text(text, open_s=171, close_s=187):
 
 
 if __name__ == '__main__':
-    print(get_quoted_text('test')
+    print(get_quoted_text('test'))
