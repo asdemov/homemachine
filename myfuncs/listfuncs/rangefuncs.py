b7362c3314616fad39ca88a0ff78ca46b31f066b
Andrey Demov <demovas@yandex.ru>
2025-09-30 17:23:51 +0300

    Функция для работы с модулем random переименована для работы с range Добавлена строка документации

--- myfuncs/listfuncs/rangefuncs.py
+++ myfuncs/listfuncs/rangefuncs.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 
 def get_num_list(high_number, low_number=0, step=-1):
+    """
+    Возвращает список чисел в убывающем порядке
+
+    :param high_number: стартовое число
+    :param low_number: конечное число
+    :param step: шаг чисел (по умолчанию числа уменьшаются на 1)
+    :return: список чисел, упорядоченный по убыванию
+    """
     return [i for i in range(high_number, low_number, step)]
 
 
