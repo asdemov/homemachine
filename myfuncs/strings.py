b1f2785e659c214d17b65d6add7a835c4f89cfa1
Andrey Demov <demovas@yandex.ru>
2025-09-30 16:31:41 +0300

    Добавлена функция, возвращающая текст в русских кавычках

---
+++ myfuncs/strings.py
@@ -0,0 +1,13 @@
+def get_quoted_text(text, open_s=171, close_s=187):
+    """
+    Возвращает слово или текст, заключенный в пару символов
+    :param text: входное слово или текст
+    :param open_s: код открывающего символа
+    :param close_s: код закрывающего символа
+    :return: текст, заключенный в символы
+    """
+    return chr(open_s) + text.strip() + chr(close_s)
+
+
+if __name__ == '__main__':
+    print(get_quoted_text('test')
