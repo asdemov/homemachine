9b2fd5379f9475ef6cb9f383f24c061f32d25958
Andrey Demov <demovas@yandex.ru>
2025-10-05 11:02:42 +0300

    Исправлены строки документации

--- myfuncs/chkfuncs/ifexsists.py
+++ myfuncs/chkfuncs/ifexsists.py
@@ -6,15 +6,17 @@ import os.path
 def is_file(file_path):
     """
     Проверяет является ли файлом переданный объект
+
     :param file_path: путь к файлу и имя файла
     :return: True или False
-    """
+        """
     return os.path.isfile(file_path)
 
 
 def is_file_exists(file_path):
     """
     Проверяет существует ли указанный файл
+
     :param file_path: путь к файлу и имя файла
     :return: True или False
     """
