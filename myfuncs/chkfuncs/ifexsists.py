09a219c62583dabe84f24c7556c0ba3fd77f6753
Andrey Demov <demovas@yandex.ru>
2025-10-05 11:01:12 +0300

    Добавлена функция is_file_exists

--- myfuncs/chkfuncs/ifexsists.py
+++ myfuncs/chkfuncs/ifexsists.py
@@ -3,9 +3,6 @@
 import os.path
 
 
-# def is_exists(file: str) -> bool:
-#     return os.path.exists(file) and os.path.isfile(file)
-
 def is_file(file_path):
     """
     Проверяет является ли файлом переданный объект
@@ -15,6 +12,15 @@ def is_file(file_path):
     return os.path.isfile(file_path)
 
 
+def is_file_exists(file_path):
+    """
+    Проверяет существует ли указанный файл
+    :param file_path: путь к файлу и имя файла
+    :return: True или False
+    """
+    return os.path.exists(file_path)
+
+
 if __name__ == '__main__':
     test = r'D:\Work\EnCase\ItemExtractor\tests.py'
-    print(is_file(test))
+    print(is_file_exists(test))
