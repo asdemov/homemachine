d4eeae1646d1239588a6ec341eb8b30f501fc9ab
Andrey Demov <demovas@yandex.ru>
2025-10-05 10:38:35 +0300

    Добавлен модуль и 2 функци  для работы с датой и временем

---
+++ myfuncs/dtfuncs/rudate.py
@@ -0,0 +1,27 @@
+﻿# -*- coding:utf-8 -*-
+# date in russian format
+from datetime import date
+
+
+def get_ru_date_list():
+    """
+    Возвращает список в формате ['dd', 'mm', 'yyyy']
+
+    :return: список
+    """
+    return [item for item in reversed(str(date.today()).split('-'))]
+
+
+def get_ru_date_from_list(splitter='.'):
+    """
+        Возвращает строку с датой в русском формате с произвольным разделителем.
+
+    :param splitter: разделитель в строке даты
+    :return: строка с датой
+        """
+    ru_date = get_ru_date_list()
+    return f'{splitter}'.join(ru_date)
+
+
+if __name__ == '__main__':
+    print(get_ru_date_from_list())
